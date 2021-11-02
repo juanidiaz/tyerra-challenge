68078d218e44ece8e3b074e98322cc7db1c4b3ea
Juan Diaz <jdi@idiaz.ca>
2021-11-02 08:45:36 -0400

    Inicio de challenge

---
+++ components/Boton.js
@@ -0,0 +1,7 @@
+export default function Boton() {
+  return (
+    <div>
+      <button type="button">Soy un boton</button>
+    </div>
+  )
+}
---
+++ pages/pagina1.js
@@ -0,0 +1,10 @@
+import Boton from "../components/Boton"
+
+export default function Pagina1() {
+  return (
+    <div>
+      <h1>Pagina 1</h1>
+      <Boton />
+    </div>
+  )
+}
